9203e513d0c1ca896af6968d0ef1d7476f0bd623
jacobharrison <jacobharrison@outlook.com>
2020-07-14 10:44:06 +0100

    add cedetails to ce new unit

--- src/main/java/uk/gov/ons/census/fwmt/common/data/ce/CENewUnitAddress.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/ce/CENewUnitAddress.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import uk.gov.ons.census.fwmt.common.data.shared.CeDetails;
 import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
 
 import java.util.UUID;
@@ -20,5 +21,7 @@ public class CENewUnitAddress extends CommonOutcome {
   @ApiModelProperty(notes = "The Site case Id (i.e. for SPG Site Id associated to a new Unit Address to be created; "
       + "if not provided, this shall be assumed to be a new Unlinked Address)")
   private UUID siteCaseId;
+
+  private CeDetails ceDetails;
 
 }
