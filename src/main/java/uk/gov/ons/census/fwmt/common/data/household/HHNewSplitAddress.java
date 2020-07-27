9ac764443c8877d55b505354da34300938b6a8b1
jacobharrison <jacobharrison@outlook.com>
2020-07-27 11:25:45 +0100

    add annotatations

--- src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
@@ -1,6 +1,16 @@
 package uk.gov.ons.census.fwmt.common.data.household;
 
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel
 public class HHNewSplitAddress extends CommonOutcome {
 }
