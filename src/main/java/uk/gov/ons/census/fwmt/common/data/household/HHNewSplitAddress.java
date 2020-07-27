c241928262207d72f603eed27b8deb3bd24676f5
jacobharrison <jacobharrison@outlook.com>
2020-07-27 11:53:14 +0100

    remove no args contrcutor annotation

--- src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
@@ -4,13 +4,11 @@ import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @ApiModel
 public class HHNewSplitAddress extends CommonOutcome {
 }
