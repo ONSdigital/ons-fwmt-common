69eb8edb37327bbc76d2840922953c1726d5b432
SteveScorfield <steve.scorfield@ons.gov.uk>
2020-09-10 15:41:11 +0100

    Added .force-concourse and added extra line to bump version in pipeline

--- src/main/java/uk/gov/ons/census/fwmt/common/retry/GatewayMessageRecover.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/retry/GatewayMessageRecover.java
@@ -5,6 +5,7 @@ import org.springframework.retry.interceptor.MethodInvocationRecoverer;
 
 public class GatewayMessageRecover implements MethodInvocationRecoverer<Void> {
 
+
   @Override
   public Void recover(Object[] args, Throwable cause) {
     throw new AmqpRejectAndDontRequeueException(cause);
