6a622214445509511cc912307832fcaa4705a53b
jacobharrison <jacobharrison@outlook.com>
2020-09-30 10:46:53 +0100

    add no args constructor annotation to hh new split address

--- src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/household/HHNewSplitAddress.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @ApiModel
 @ToString(callSuper = true)
 public class HHNewSplitAddress extends CommonOutcome {
