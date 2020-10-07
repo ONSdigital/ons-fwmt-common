2c0124a6311b21e8402a07966ff731d650ed1de1
jacobharrison <jacobharrison@outlook.com>
2020-10-07 15:52:46 +0100

    add refusal to object

---
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/shared/Refusal.java
@@ -0,0 +1,36 @@
+package uk.gov.ons.census.fwmt.common.data.shared;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel
+@ToString
+public class Refusal {
+
+  @ApiModelProperty(notes = "")
+  private String title;
+
+  @ApiModelProperty(notes = "")
+  private String firstname;
+
+  @ApiModelProperty(notes = "")
+  private String surname;
+
+  @ApiModelProperty(notes = "")
+  private String middlename;
+
+  @ApiModelProperty(notes = "")
+  private boolean householder;
+
+  @ApiModelProperty(notes = "")
+  private boolean dangerous;
+
+
+}
