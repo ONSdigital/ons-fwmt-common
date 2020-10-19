3b3e3ee9172f8c253f93e330bcfbab86ea78f9bf
jacobharrison <jacobharrison@outlook.com>
2020-10-19 14:18:40 +0100

    chage for ccs

--- src/main/java/uk/gov/ons/census/fwmt/common/data/ccs/CCSPropertyListingOutcome.java
+++ src/main/java/uk/gov/ons/census/fwmt/common/data/ccs/CCSPropertyListingOutcome.java
@@ -7,22 +7,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 import uk.gov.ons.census.fwmt.common.data.shared.CeDetails;
 import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
+import uk.gov.ons.census.fwmt.common.data.shared.FulfilmentRequest;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel
 @ToString
-public class CCSPropertyListingOutcome {
-
-  @ApiModelProperty(notes = "Unique identifier for the request", required = true)
-  private UUID transactionId;
 public class CCSPropertyListingOutcome extends CommonOutcome {
 
   @ApiModelProperty(notes = "Flag to indicate whether Dummy Information has been collected")
@@ -31,17 +25,7 @@ public class CCSPropertyListingOutcome extends CommonOutcome {
   @ApiModelProperty(notes = "Populated with the CE SPG details")
   private CeDetails ceDetails;
 
-  @ApiModelProperty(notes = "Access/security info - not used for 2019;\n"
-      + " pre-empting the need to be able to create a CE case from a HH case in 2021.")
-  private String accessInfo;
-
-  @ApiModelProperty(notes = "Care Codes applied to the case, can be 0, 1 or more of:\n"
-      + "Access control\n"
-      + "Aggressive animal\n"
-      + "Caution required")
-  private List<CareCode> careCodes;
-
   @ApiModelProperty(notes = "The CCSPropertyListingOutcome may include a child FulfillmentRequest object.")
-  private List<FulfillmentRequest> fulfilmentRequests = null;
+  private List<FulfilmentRequest> fulfilmentRequests = null;
 
 }
